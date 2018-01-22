e811558b66570a366acee19e1522cf63659f1aa9
Armin <armin@Armin-Mac.local>
2018-01-22 11:18:18 +0330

    Initial Commit

---
+++ project/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  project
+//
+//  Created by Armin on 1/22/18.
+//  Copyright © 2018 Armin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ project/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Armin on 1/22/18.
+//  Copyright © 2018 Armin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
